9b51a986ed843f19bd899349c60fd36803697df3
Ashish_Nagdev <ashunagdev@gmail.com>
2017-01-04 02:02:04 -0500

    refactoring to code

--- src/main/scala/com/sherpa/examples/sparkdemo/SparkSessionExample.scala
+++ src/main/scala/com/sherpa/examples/sparkdemo/SparkSessionExample.scala
@@ -3,21 +3,21 @@ package com.sherpa.examples.sparkdemo
 import org.apache.spark.sql.SparkSession
 
 /**
+  * Created by Ashish Nagdev
   * Spark Session example
-  *
   */
-object SparkSessionExample {
+object SparkSessionSample {
 
   def main(args: Array[String]) {
 
-    val sparkSession = SparkSession.builder.
-      master("local")
+    val sparkSession = SparkSession.builder
+      .master("local")
       .appName("spark session example")
       .getOrCreate()
 
-    val df = sparkSession.read.option("header","true").csv("src/main/resources/sales.csv")
+    val dataFrame = sparkSession.read.option("header", "true").csv("src/main/resources/sales.csv")
 
-    df.show()
+    dataFrame.show()
 
   }
 
